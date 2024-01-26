1c1ecf618b296520f1846cc060ad1adf6ece0b45
Luckyjaiswal <110555155+Luckyjaiswal@users.noreply.github.com>
2024-01-26 21:31:57 +0530

    first commit

---
+++ src/Data/data.js
@@ -0,0 +1,46 @@
+import Resume1 from '../Templetes/Resume1'
+import Resume2 from '../Templetes/Resume2'
+import Resume3 from '../Templetes/Resume3'
+import Resume4 from '../Templetes/Resume4'
+import Resume5 from '../Templetes/Resume5'
+import Resume6 from '../Templetes/Resume6'
+import Resume7 from '../Templetes/Resume7'
+import Resume8 from '../Templetes/Resume8'
+
+// Stores All Templates As a json State
+
+const temp =[
+{
+thumbnail:'/images/Templete1.png',
+data:<Resume1/>},
+
+{
+thumbnail:'/images/Templete2.png',
+data:<Resume2/>},
+
+{
+thumbnail:'/images/Templete3.png',
+data:<Resume3/>},
+
+{
+thumbnail:'/images/Templete4.jpg',
+data:<Resume4/>},
+
+{
+    thumbnail:'/images/Templete5.jpg',
+    data:<Resume5/>},
+
+{
+    thumbnail:'/images/Templete6.jpg',
+    data:<Resume6/>},   
+
+{
+    thumbnail:'/images/Templete7.jpg',
+    data:<Resume7/>},  
+        
+{
+    thumbnail:'/images/Templete8.jpg',
+    data:<Resume8/>},          
+]
+
+export default temp ;
